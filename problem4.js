c2bfba63ae23cdff85aac61788d92266f67160c5
Anik8118 <156513437+Anik8118@users.noreply.github.com>
2024-10-24 20:43:36 +0600

    Forth Problem

---
+++ problem4.js
@@ -0,0 +1,23 @@
+/*Problem 4:
+Write a function isPalindrome that takes in a string and returns true if the string is a palindrome (reads the same forwards and backwards) and false otherwise.
+Example Input/Output:
+isPalindrome("racecar") should return true
+isPalindrome("hello") should return false
+isPalindrome("rotator") should return true
+isPalindrome("peep") should return true*/
+
+function isPalindrome(str){
+    let palindrom='';
+    for(i=str.length-1;i>=0;i--){
+        palindrom+=str[i];
+    }
+    if(palindrom==str){
+        return true;
+    }else{
+        return false
+    }
+}
+
+console.log(isPalindrome("racecar"));  // Output: true
+console.log(isPalindrome("rotator"));  // Output: true
+console.log(isPalindrome("hello"));    // Output: false
